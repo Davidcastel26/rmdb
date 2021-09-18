df8fa4a1ce5746bb761b65d1ab0ea42972fbbe12
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-09-18 13:49:38 -0600

    create the space for the hero

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -19,6 +19,8 @@ const Home = () => {
             { state.results[0] ? 
             <HeroImage 
                 image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
             />
             : null
             }
