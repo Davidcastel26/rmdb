99bff3236bfee5f2daffa513630f178c7fec94dc
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-09-19 15:19:37 -0600

    the style from the component is done

--- react-rmdb/src/components/Thumb/index.js
+++ react-rmdb/src/components/Thumb/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // Styles
 import {Image } from './Thumb.styles';
-
+ 
 const Thumb = ({image, movieId, clickable}) => ( 
     <div>
         <Image src={image} alt='movie-thumb' />
