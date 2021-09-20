5ac55fc6e7533735ee3c9003924babdfe44fe3e3
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-09-19 18:09:32 -0600

    importing the seachbar styles

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -8,12 +8,13 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const { state, loading, error } = useHomeFetch();
+    const { state, loading, error, setSearchTerm } = useHomeFetch();
 
     console.log(state);
 
@@ -27,6 +28,9 @@ const Home = () => {
             />
             : null
             }
+            <SearchBar 
+                setSearchTerm={setSearchTerm}
+            />
             <Grid header='Popular Movies'>
                 {state.results.map(movie=>(
                     <Thumb 
